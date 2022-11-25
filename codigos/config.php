a564eebc5b726637b5dc08cfb0beda7e3e3d1353
Eduardo Chassot <dudunaboa@outlook.com>
2022-11-25 17:37:44 -0300

    [new] consultar

--- codigos/config.php
+++ codigos/config.php
@@ -2,7 +2,26 @@
 
 $user = 'root';
 $password = '';
-$database = '';
-$hostname = '';
+$database = 'storage';
+$hostname = 'localhost';
+
+function consultar($query, $echo = 0){
+
+    $connect = mysqli_connect($hostname, $user, $password, $database);
+
+    if($echo = 1){
+        echo $query;
+    }
+
+    $search = mysqli_query($connect, $query);
+    while($loop = mysqli_fetch_assoc($search)){
+        $return[] = $loop;
+    }
+
+    mysqli_close($connect);
+
+    return $return;
+
+}
 
 ?>
