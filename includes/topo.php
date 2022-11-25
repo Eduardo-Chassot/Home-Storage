420e25d1c7991c71e3aa5a12ad5a42b9a9c5e999
Gustavo Nienov <henriquenienov@gmail.com>
2022-11-25 17:53:19 -0300

    [new] front

---
+++ includes/topo.php
@@ -0,0 +1,8 @@
+<html>
+<link rel="stylesheet" type="text/css" href="style.css">
+    <div>
+        <input type="text" id="busca" name="busca">
+        <button class="home"></button>
+        <button></button>
+    </div>
+</html>
