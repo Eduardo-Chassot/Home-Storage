8f1c37f741c56c60236611ea8cef7e1298dc8e13
Gustavo Nienov <henriquenienov@gmail.com>
2022-11-25 18:46:37 -0300

    continuacao

--- includes/topo.php
+++ includes/topo.php
@@ -2,9 +2,9 @@
 <link rel="stylesheet" type="text/css" href="../style.css">
 <body>
     <div class='cabecalho'>
-        <input type="text" id="busca" name="busca" placeholder='Pesquisar'>
-        <button class="home"></button>
-        <button></button>
+        <input type="text" id="busca" name="busca" placeholder='Pesquisar' style="margin-top:10px; margin-left:1%; margin-right:70%;">
+        <input class="home" type="image" src="https://cdn-icons-png.flaticon.com/512/25/25694.png" style="max-widht:32px; max-height:32px;">
+        <input class="add" type="image" src="https://cdn-icons-png.flaticon.com/512/32/32339.png" style="max-widht:32px; max-height:32px;">
     </div>
 </body>
 </html>
