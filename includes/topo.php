fbfbeac3c54b64f9587730c4b6202041d7a373f7
Gustavo Nienov <henriquenienov@gmail.com>
2022-11-25 18:01:46 -0300

    atualizacao

--- includes/topo.php
+++ includes/topo.php
@@ -1,8 +1,10 @@
 <html>
-<link rel="stylesheet" type="text/css" href="style.css">
-    <div>
-        <input type="text" id="busca" name="busca">
+<link rel="stylesheet" type="text/css" href="../style.css">
+<body>
+    <div class='cabecalho'>
+        <input type="text" id="busca" name="busca" placeholder='Pesquisar'>
         <button class="home"></button>
         <button></button>
     </div>
+</body>
 </html>
