a680de51eba3f99168efe160b9a3e1e14abaa48a
Gustavo Nienov <henriquenienov@gmail.com>
2022-12-02 17:37:52 -0300

    atualizações

--- includes/topo.php
+++ includes/topo.php
@@ -2,9 +2,9 @@
 <link rel="stylesheet" type="text/css" href="../style.css">
 <body>
     <div class='cabecalho'>
-        <input type="text" id="busca" name="busca" placeholder='Pesquisar' style="margin-top:10px; margin-left:1%; margin-right:70%;">
-        <input class="home" type="image" src="https://cdn-icons-png.flaticon.com/512/25/25694.png" style="max-widht:32px; max-height:32px;">
-        <input class="add" type="image" src="https://cdn-icons-png.flaticon.com/512/32/32339.png" style="max-widht:32px; max-height:32px;">
+        <input type="text" id="busca" name="busca" placeholder='Pesquisar' style="margin-top:10px; margin-left:1%; margin-right:20%">
+        <input class="home" title="Ir para pagina inicial" type="image" src="https://cdn-icons-png.flaticon.com/512/25/25694.png" style="max-widht:32px; max-height:32px;">
+        <input class="add" title="Adicionar produto" type="image" src="https://cdn-icons-png.flaticon.com/512/32/32339.png" style="max-widht:32px; max-height:32px;">
     </div>
 </body>
 </html>
